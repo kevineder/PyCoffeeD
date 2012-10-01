8518862b8c106a64ff93481f692051bc0d920c5f
Raspberry Pi User <pi@raspberrypi.(none)>
2012-10-01 01:22:45 +0100

    Adding a few new endpoints

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import json
 from flask import Response
+from flask import send_from_directory
+import os
 
 import sys
 sys.path.append(sys.path[0]) 
-from coffeed import Scale
+from Scale import Scale
 
 VENDOR_ID = 0x6096
 PRODUCT_ID = 0x0158
@@ -31,7 +33,27 @@ def cups():
 	resp = Response(js, status=200, mimetype='application/json')
 	return resp
 
+@app.route("/servings")
+def servings():
+	js = json.dumps(scale.readOunces()/6.0)
+	resp = Response(js, status=200, mimetype='application/json')
+	return resp
+
+@app.route("/caffeine")
+def caffeine():
+	js = json.dumps(str(round((scale.readOunces()/8.5)*49,2)) + "mg")
+	resp = Response(js, status=200, mimetype='application/json')
+	return resp
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'),
+		'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.errorhandler(418)
+def error418():
+	return "I am a tea pot"
+
 if __name__ == "__main__":
 	scale = Scale(VENDOR_ID, PRODUCT_ID)
 	app.run(host='0.0.0.0')
-

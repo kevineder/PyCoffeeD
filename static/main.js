e94fc1a86c442f9b0c7e066da685cd685da681a9
Kevin Eder <kevin.eder@gmail.com>
2012-10-09 09:22:47 -0400

    changing serving sizes to 5 oz.

--- static/main.js
+++ static/main.js
@@ -31,7 +31,7 @@ function updateStats() {
       }
 
       servings = Math.round(100 * servings)/100;
-      var caffeine = Math.round(100 * ((servings * 6)/8.5) * 49)/100;
+      var caffeine = Math.round(100 * ((servings * 5)/8.5) * 49)/100;
 
       $(".hero-unit h1").text("There are " + servings + " cups of coffee left.");
       $(".hero-unit h2").text("(That's ~" + caffeine + "mg of caffeine.)");
